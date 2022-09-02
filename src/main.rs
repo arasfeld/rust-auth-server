609393a25d3c122458ca0b3a36b1b240d56c530a
Adam Rasfeld <arasfeld@gmail.com>
2022-09-01 23:26:15 -0400

    add sqlx postgres

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use axum::{
     http::{HeaderValue, Method},
     response::Html,
     routing::get,
-    Router
+    Extension, Router
 };
-use std::net::SocketAddr;
+use sqlx::postgres::{PgPoolOptions};
+use std::{net::SocketAddr, time::Duration};
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -24,11 +25,22 @@ async fn main() {
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET]);
 
+    let db_connection_str = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost:5432".to_string());
+
+    let pool = PgPoolOptions::new()
+        .max_connections(50)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("cannot connect to database");
+
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
         .layer(TraceLayer::new_for_http())
-        .layer(cors);
+        .layer(cors)
+        .layer(Extension(pool));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
